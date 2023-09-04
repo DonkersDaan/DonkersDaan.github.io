142a61ed5a856a84ada3faf00d66be5aa5991366
Daan Donkers <57719535+DonkersDaan@users.noreply.github.com>
2023-09-04 19:28:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -78,6 +78,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Update the textRight element based on the button's index
             textRight.innerHTML = getPageText(buttonIndex);
+            media.scrollTop = 0;
+
         });
     });
 
@@ -154,9 +156,6 @@ function getButtonText(buttonIndex) {
             return "Climate";
         case '4':
             return "Other";
-        // case '5':
-        //     return "Contact";
-
         default:
             return "";
     }
@@ -169,26 +168,21 @@ function getPageText(buttonIndex) {
             changeHome();
             header.innerHTML = "Welcome!";
             return "My name is Daan Donkers, and I'm a junior Interaction Designer with a very broad interest in the media & tech world! Please take a look at some of my work!<br><br>If you want to know more about my work, or just anything else, feel free to contact me!<br><br><u>Email</u>: daan.donkers@hotmail.nl<br><br>* <b>Specialization</b>: Game Design & Technology<br>* <b>Minor</b>: Transmedia Design & Storytelling<br><br><br><br><i>If you click on the prism, the background color will change. Change it according to your liking.</i>";
-
         case '1':
             ARBook();
             header.innerHTML = "AR-Photobook";
-            return "<b>What is it?</b><br>An AR Photobook with personalised minigames and messages for my girlfriend, made in Unity with ARCore, and Vuforia.<br><br><b>The story</b><br>This photobook is no ordinary book. It contains 4 special pictures, that each have a 'hidden star'. Each star represents a memory I hold dear with my girlfriend. Moreover, each star has a fitting minigame that unlocks a special thanks from me to her after completion. <br><br><b>Why this book?</b><br>My girlfriend likes photography. Her favorite song is 'Yellow' from Coldplay. And I want to show in 4 sentences how much I appreciate her. Together with my Unity skills and creative mind, I wanted to do something special for our 2nd anniversary.";
+            return "<b>What is it?</b><br>An AR Photobook with personalised minigames and messages for my girlfriend, made in Unity with ARCore, and Vuforia.<br><br><b>The story</b><br>This photobook is no ordinary book. It contains 4 special pictures, that each have a 'hidden star'. Each star represents a memory I hold dear with my girlfriend. Moreover, each star has a fitting minigame that unlocks a special thanks from me to her after completion. <br><br><b>Why this book?</b><br>My girlfriend likes photography. Her favorite song is 'Yellow' from Coldplay. And I want to show in 4 sentences how much I appreciate her. Together with my Unity skills and creative mind, I wanted to do something special for our 2nd anniversary.<br><br><i>Hover over the video to pause.</i>";
         case '2':
             changeDefence();
             header.innerHTML = "Autism Experience Game";
-            return "To the left are some other school and passion projects<br><i>Hover over an image to show what it is about</i>";
+            return "<i>Hover over an image to show what it is about.</i>";
         case '3':
             changeGAG();
             header.innerHTML = "Gamble against God";
-            return "Text for button 4";
+            return "<i>Hover over an image to show what it is about.</i>";
         case '4':
             Others();
-            return "To the left are some other school and passion projects<br><i>Hover over an image to show what it is about</i>";
-        // case '5':
-
-        //     return "Text for button 6";
-        // Add more cases for other button indexes
+            return "To the left are some other school and passion projects<br><i>Hover over an image to show what it is about.</i>";
         default:
             return "";
     }
@@ -197,6 +191,9 @@ function getPageText(buttonIndex) {
 //Change Content
 
 function changeHome() {
+
+    video.src = "";
+    video.style.display = "none";
 
     media.style.justifyContent = "center";
     media.style.alignItems = "center";
@@ -211,77 +208,33 @@ function changeHome() {
             image.style.display = "none"; // Deactivate other images
         }
     });
-
-    video.src = "";
-    video.style.display = "none";
 }
 
 function changeDefence() {
-
-    // media.style.justifyContent = "center";
-    // media.style.alignItems = "center";
-    // media.style.flexDirection = "";
-    // media.style.flexWrap = ""; // Allow images to wrap to the next row
-
-    // images.forEach((image, index) => {
-    //     if (index === 1) {
-    //         image.style.display = "inline"; // Activate the second image
-    //         image.style.width = "";
-    //     } else {
-    //         image.style.display = "none"; // Deactivate other images
-    //     }
-    // });
-
     video.src = "";
     video.style.display = "none";
 
-    // header.innerHTML = "Other";
-    // textRight.innerHTML = "";
-
     images.forEach((image, index) => {
-        if (index >= 1 && index <= 6) {
+        if (index >= 1 && index <= 7) {
             image.style.display = "inline"; // Activate the second image
             image.classList.add("scrollindication");
-            // image.style.width = "45%";
 
-            images[1].addEventListener("mouseover", () => {
-                image.classList.remove("scrollindication");
-                textRight.innerHTML = getImageTextDefence(1);
+            image.addEventListener("mouseover", () => {
+                textRight.innerHTML = getImageText(index, 'defence');
+                images.forEach((img) => {
+                    img.classList.remove("scrollindication");
+                })
             });
-
-
-            images[2].addEventListener("mouseover", () => {
-                textRight.innerHTML = getImageTextDefence(2);
-            });
-
-
-            images[3].addEventListener("mouseover", () => {
-                textRight.innerHTML = getImageTextDefence(3);
-            });
-
-
-            images[4].addEventListener("mouseover", () => {
-                textRight.innerHTML = getImageTextDefence(4);
-            });
-
-            images[5].addEventListener("mouseover", () => {
-                textRight.innerHTML = getImageTextDefence(5);
-            });
-
-
-            images[6].addEventListener("mouseover", () => {
-                textRight.innerHTML = getImageTextDefence(6);
-            });
-
 
         } else {
             image.style.display = "none"; // Deactivate other images
         }
 
+        // text.addEventListener("mouseover", () => {
+        //     image.classList.add("scrollindication");
+        // });
+
     });
-
-
-    // Modify CSS properties
 
     media.style.justifyContent = "";
     media.style.alignItems = "flex-start";
@@ -292,67 +245,55 @@ function changeDefence() {
 }
 
 // Function to get corresponding text for each image
-function getImageTextDefence(index) {
-    switch (index) {
-        case 1:
-            return "<b>What is it?</b><br>A WebGL game in which the user experiences multiple different situations, through the eyes of different people with autism. The game is based on the stories and experiences of a variety of Defence personnel with autism. It is made in Unity and used by our military.<br><br><b>The story</b><br>The Autism Experience Game is a discovery game focused on the differences in perception between people with and without autism. Play unique scenarios that allow you to experience the contrast between those two worlds. Reflect on your experiences, and learn how to navigate them.<br><br><b>Why this game?</b><br>People with autism are at a high risk of experiencing burnout, depression, and workplace absenteeism. Communication problems (partly caused by a misunderstanding of autism) can lead to conflicts at work. Colleagues with autism within the military often feel misunderstood. Their peers express that they do not know how to handle certain situations.<br><br><b>After completion</b><br>The game was a succes, and caused for many collegues to have a better understanding on the strenghts and difficulties autism can give someone, but also on how to put those strenghts to good use. The game also helped some people with autism to better talk about their insecurities or problems, and it even played a role in getting our Ministry of Defence its own Autism Embassy.<br><br><b>My job</b> (first internship)<br>* Concept developer<br>* UX + UI Designer/ Developer<br>* Researcher<br>* Lead<br><br>";
-        case 2:
-            return "Home menu, choosing scenario.";
-        case 3:
-            return "Play as someone with autism. You have no controle over the situation.";
-        case 4:
-            return "There is no right answer.";
-        case 5:
-            return "You learned from the experience by playing the game in his or her shoes.";
-        case 6:
-            return "You know what you need to make the situation work for you.";
-        default:
-            return "";
-    }
-}
 
 function changeGAG() {
-
-    media.style.justifyContent = "center";
-    media.style.alignItems = "center";
-    media.style.flexDirection = "";
-    media.style.flexWrap = ""; // Allow images to wrap to the next row
-
-    header.innerHTML = "Welcome!";
-    textRight.innerHTML = "<p> My name is Daan Donkers, and I'm a junior Interaction Designer with a very broad interest in the media & tech world! Please take a look at my work.<br><br><i>If you click on the prism, the background color will change. Change it according to your liking.</i><h1>";
-    //Add the 6 buttons. Each button will get a ++number or a bool false or true that changes the image to the correct image.
+    video.src = "";
+    video.style.display = "none";
 
     images.forEach((image, index) => {
-        if (index === 7) {
+        if (index >= 8 && index <= 11) {
             image.style.display = "inline"; // Activate the second image
+            image.classList.add("scrollindication");
+
+            image.addEventListener("mouseover", () => {
+                textRight.innerHTML = getImageText(index, 'GAG');
+                images.forEach((img) => {
+                    img.classList.remove("scrollindication");
+                    console.log("Hovering over image", index);
+                })
+            });
+
         } else {
             image.style.display = "none"; // Deactivate other images
         }
+
     });
 
+    media.style.justifyContent = "";
+    media.style.alignItems = "flex-start";
+    media.style.flexDirection = "row";
+    media.style.flexWrap = "wrap";
 
-    video.src = "";
-    video.style.display = "none";
+    images[8].addEventListener("mouseover", () => {
+        images[8].src = "./BestProjects/LogoFinal-Green.png";
+    });
+
+    images[8].addEventListener("mouseleave", () => {
+        images[8].src = "./BestProjects/LogoFinal-Blue.png";
+    });
+
 }
 
-
 function ARBook() {
-
     media.style.justifyContent = "center";
     media.style.alignItems = "center";
     media.style.flexDirection = "";
-    media.style.flexWrap = ""; // Allow images to wrap to the next row
-
-
-
+    media.style.flexWrap = "";
 
     images.forEach((image) => {
         image.style.display = "none";
     });
 
-
-    // image.src = "";
-    //Add the 6 buttons. Each button will get a ++number or a bool false or true that changes the image to the correct image.
     video.src = "./BenteDaanVid1.mp4";
 
     video.style.display = "initial";
@@ -370,18 +311,21 @@ function ARBook() {
 
 }
 
-
 function Others() {
     header.innerHTML = "Other";
     textRight.innerHTML = "";
 
     images.forEach((image, index) => {
-        if (index >= 8 && index <= 11) {
-            image.style.display = "inline"; // Activate the second image
+        if (index >= 12 && index <= 15) {
+            image.style.display = "inline";
             image.style.width = "45%";
 
             image.addEventListener("mouseover", () => {
-                textRight.innerHTML = getImageText(index);
+                textRight.innerHTML = getImageText(index, 'other');
+                images.forEach((img) => {
+                    img.classList.remove("scrollindication");
+                    console.log("Hovering over image", index);
+                })
             });
 
             image.addEventListener("mouseleave", () => {
@@ -389,12 +333,9 @@ function Others() {
             });
 
         } else {
-            image.style.display = "none"; // Deactivate other images
+            image.style.display = "none";
         }
     });
-
-
-    // Modify CSS properties
 
     media.style.justifyContent = "";
     media.style.alignItems = "flex-start";
@@ -403,24 +344,60 @@ function Others() {
 
     video.src = "";
 
-
 }
 
 // Function to get corresponding text for each image
-function getImageText(index) {
-    switch (index) {
-        case 3:
-            return "Text for image 3";
-        case 4:
-            return "Text for image 4";
-        case 5:
-            return "Text for image 5";
-        case 6:
-            return "Text for image 6";
-        default:
-            return "";
+function getImageText(index, category) {
+    switch (category) {
+        case 'defence':
+            switch (index) {
+                // case 0:
+                //     return "My name is Daan Donkers, and I'm a junior Interaction Designer with a very broad interest in the media & tech world! Please take a look at some of my work!<br><br>If you want to know more about my work, or just anything else, feel free to contact me!<br><br><u>Email</u>: daan.donkers@hotmail.nl<br><br>* <b>Specialization</b>: Game Design & Technology<br>* <b>Minor</b>: Transmedia Design & Storytelling<br><br><br><br><i>If you click on the prism, the background color will change. Change it according to your liking.</i>";
+                case 1:
+                    return "<b>What is it?</b><br>A WebGL game in which the user experiences multiple different situations, through the eyes of different people with autism. The game is based on the stories and experiences of a variety of Defence personnel with autism. It is made in Unity and used by our military.<br><br><b>The story</b><br>The Autism Experience Game is a discovery game focused on the differences in perception between people with and without autism. Play unique scenarios that allow you to experience the contrast between those two worlds. Reflect on your experiences, and learn how to navigate them.<br><br><b>Why this game?</b><br>People with autism are at a high risk of experiencing burnout, depression, and workplace absenteeism. Communication problems (partly caused by a misunderstanding of autism) can lead to conflicts at work. Colleagues with autism within the military often feel misunderstood. Their peers express that they do not know how to handle certain situations.<br><br><b>After completion</b><br>The game was a succes, and caused for many collegues to have a better understanding on the strenghts and difficulties autism can give someone, but also on how to put those strenghts to good use. The game also helped some people with autism to better talk about their insecurities or problems, and it even played a role in getting our Ministry of Defence its own Autism Embassy.<br><br><b>My job</b> (first internship)<br>* Concept developer<br>* UX + UI Designer/ Developer<br>* Researcher<br>* Lead<br><br>";
+                case 2:
+                    return "Home menu. Choose a scenario.";
+                case 3:
+                    return "Play as someone with autism. You have no controle over the situation. This is due to internal, and external factors that make it almost impossible for the player to play the game correctly.";
+                case 4:
+                    return "There is no right answer to questions that come your way.";
+                case 5:
+                    return "You learned from the stressfull experience(s) by playing the game in his or her shoes.";
+                case 6:
+                    return "You know what you need, to make the situation work for you.";
+                case 7:
+                    return "You reflect on the experience and learn about the strengths and weaknesses of your collegue with autism.";
+                default:
+                    return "";
+            }
+        case 'GAG':
+            switch (index) {
+                case 8:
+                    return "<b>What is it?</b><br>A WebGL game in which the user experiences multiple different situations, through the eyes of different people with autism. The game is based on the stories and experiences of a variety of Defence personnel with autism. It is made in Unity and used by our military.<br><br><b>The story</b><br>The Autism Experience Game is a discovery game focused on the differences in perception between people with and without autism. Play unique scenarios that allow you to experience the contrast between those two worlds. Reflect on your experiences, and learn how to navigate them.<br><br><b>Why this game?</b><br>People with autism are at a high risk of experiencing burnout, depression, and workplace absenteeism. Communication problems (partly caused by a misunderstanding of autism) can lead to conflicts at work. Colleagues with autism within the military often feel misunderstood. Their peers express that they do not know how to handle certain situations.<br><br><b>After completion</b><br>The game was a succes, and caused for many collegues to have a better understanding on the strenghts and difficulties autism can give someone, but also on how to put those strenghts to good use. The game also helped some people with autism to better talk about their insecurities or problems, and it even played a role in getting our Ministry of Defence its own Autism Embassy.<br><br><b>My job</b> (first internship)<br>* Concept developer<br>* UX + UI Designer/ Developer<br>* Researcher<br>* Lead<br><br>";
+                case 9:
+                    return "Home menu. Choose a scenario.";
+                case 10:
+                    return "Play as someone with autism. You have no controle over the situation. This is due to internal, and external factors that make it almost impossible for the player to play the game correctly.";
+                case 11:
+                    return "There is no right answer to questions that come your way.";
+                default:
+                    return "";
+            }
+        case 'other':
+            switch (index) {
+                case 12:
+                    return "Text for image 3";
+                case 13:
+                    return "Text for image 4";
+                case 14:
+                    return "Text for image 5";
+                case 15:
+                    return "Text for image 6";
+                default:
+                    return "";
+            }
     }
-}
 
+}
 
 
